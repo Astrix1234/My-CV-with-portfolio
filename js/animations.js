10f0be3a447b07cd1b0907c219e108949972f157
Asia <dobrowolska.krakow@gmail.com>
2024-01-09 20:22:50 +0100

    Working on improving graphics

--- js/animations.js
+++ js/animations.js
@@ -5,3 +5,9 @@ const letters = document.querySelectorAll('.portfolio__title span');
 letters.forEach((letter, index) => {
   letter.style.animationDelay = `${index * 0.2}s`;
 });
+
+document.querySelectorAll('.text__see-more').forEach(button => {
+  button.addEventListener('click', function () {
+    this.parentElement.classList.toggle('expanded');
+  });
+});
