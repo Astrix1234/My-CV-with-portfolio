7d94e2a9f1aafe0a51260deaa294711ba1e1ed51
Asia <dobrowolska.krakow@gmail.com>
2024-01-09 13:04:13 +0100

    Working on improving graphics

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,23 +3,17 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  const mobilLinks = document.querySelectorAll('.mobile-nav__link');
 
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
-    const scrollLockMethod = !isMenuOpen
-      ? 'disableBodyScroll'
-      : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  mobilLinks.forEach(link => link.addEventListener('click', toggleMenu));
+  mobileMenu.addEventListener('click', toggleMenu);
 
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
